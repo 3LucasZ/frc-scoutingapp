9bc23e8085151fec10d093c8c2af113a359db61a
3LucasZ <lucas.j.zheng@gmail.com>
2021-10-27 18:44:48 -0700

    CORS debug

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import random
 
 #setup
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scouting-report.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
